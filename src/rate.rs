96532e177025ecdc3ac9432f3292bf0c4a3b05b4
mossa <cgmossa@gmail.com>
2023-10-12 10:38:42 +0200

    added rate type

---
+++ src/rate.rs
@@ -0,0 +1,27 @@
+use derive_more::*;
+
+#[derive(Debug, PartialEq, PartialOrd, Into, Add, AddAssign)]
+pub struct Rate(pub f64);
+
+impl Default for Rate {
+    fn default() -> Self {
+        Self(0.)
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("cannot create a rate from negative number, instead {:?}", .0)]
+    NegativeRate(f64),
+}
+
+impl TryFrom<f64> for Rate {
+    type Error = Error;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        value
+            .is_sign_positive()
+            .then(|| Self(value))
+            .ok_or_else(|| Error::NegativeRate(value))
+    }
+}
